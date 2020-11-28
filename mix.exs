2d3f6a3e25e31d526651ee3d06365a93fced35b4
okoth kongo <34419890+okothkongo@users.noreply.github.com>
2020-11-28 11:57:58 +0300

    fix sentry deps (#22)

--- mix.exs
+++ mix.exs
@@ -56,7 +56,8 @@ defmodule SimpleHome.MixProject do
       {:excoveralls, "~> 0.13.3", only: :test},
       {:phoenix_integration, "~> 0.8.2"},
       {:wallaby, "~> 0.26.0", runtime: false, only: :test},
-      {:sentry, "~> 8.0"}
+      {:sentry, "~> 8.0"},
+      {:hackney, "~> 1.8"}
     ]
   end
 
